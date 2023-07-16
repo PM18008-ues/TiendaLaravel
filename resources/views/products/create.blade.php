ef6b980b68e96c8e9631d41bffd57d99aa2f01bc
Melvin Portillo <melvin@meliodasfury.melvin>
2023-07-15 19:02:10 -0600

    create product views

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Crear producto
+                    </div>
+                    <div class="card-body">
+                        <form action="">
+                            <div class="form-group">
+                                <label for="description">Descripcion</label>
+                                <input type="text" class="form-control" name="description">
+                            </div>
+                            <div class="form-group">
+                                <label for="price">Precio</label>
+                                <input type="number" class="form-control" name="price">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                            <a href="{{ route('products.index') }}" class="btn btn-danger">Cancelar</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Listado de productos
+                        <a href="{{ route('products.create')}}" class="btn btn-success btn-sm float-end">Nuevo Producto</a>
+                    </div>
+                    <div class="card-body">
+                        ...
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
