7250817c4d9a082702f8c02671535d97b2667da1
Melvin Portillo <melvin@meliodasfury.melvin>
2023-07-15 19:03:33 -0600

    create product views

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('products', function () {
+    return view('products.index');
+})->name('products.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('products/create', function () {
+    return view('products.create');
+})->name('products.create');
